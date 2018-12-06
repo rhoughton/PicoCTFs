525eef43bae27fe9a3da1481e61b9b1237491254
Rob <houghton.robert.e@gmail.com>
2018-12-06 09:36:08 -0700

    Create assembly0.s

---
+++ assembly0.s
@@ -0,0 +1,22 @@
+.intel_syntax noprefix
+.bits 32
+	
+.global asm0
+
+asm0:
+	push	ebp ;function prologue
+	mov	ebp,esp ;same
+	mov	eax,DWORD PTR [ebp+0x8] ;ebp is a base pointer, eax = 0x2a
+	mov	ebx,DWORD PTR [ebp+0xc] ;ebx = 0x4f
+	mov	eax,ebx ; eax = ebx = 0x4f
+	mov	esp,ebp ;function epilogue
+	pop	ebp	
+	ret
+  
+  asm0(0x2a, 0x4f)
+
+/stack stuff
+arg
+ret addr
+ebp
+local variables
